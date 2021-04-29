83150ab52887f44fe2cd598839f5aa97ffe0955c
alex <fancn21th@aliyun.com>
2021-04-29 18:10:58 +0800

    show hierarchy views

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -17,7 +17,8 @@ Description related to library.
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml'
+        'views/library_book.xml',
+        'views/library_book_categ.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
