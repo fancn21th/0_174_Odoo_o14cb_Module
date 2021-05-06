69a92de6ffdfc09d1bc752827512a4c1c2a748a8
alex <fancn21th@aliyun.com>
2021-05-06 21:12:21 +0800

    filtering recordsets

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -95,6 +95,20 @@ class LibraryBook(models.Model):
         logger.info('Books found: %s', books)
         return True
 
+    # Filter recordset
+    def filter_books(self):
+        all_books = self.search([])
+        filtered_books = self.books_with_multiple_authors(all_books)
+        logger.info('Filtered Books: %s', filtered_books)
+
+    @api.model
+    def books_with_multiple_authors(self, all_books):
+        def predicate(book):
+            if len(book.author_ids) >= 1:
+                return True
+        return all_books.filtered(predicate)
+
+
 class LibraryMember(models.Model):
 
     _name = 'library.member'
