161ab663e840b79d2ee567f90ebad15ce88ab863
alex <fancn21th@aliyun.com>
2021-07-07 18:19:50 +0800

    re-do assets_frontend

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -9,15 +9,3 @@ class LibraryBook(models.Model):
     name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
-    image = fields.Binary(attachment=True)
-    html_description = fields.Html()
-    book_issue_ids = fields.One2many('book.issue', 'book_id')
-
-
-class LibraryBookIssues(models.Model):
-    _name = 'book.issue'
-
-    book_id = fields.Many2one('library.book', required=True)
-    submitted_by = fields.Many2one('res.users')
-    issue_description = fields.Text()
-
