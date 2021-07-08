1e5422f26313b7c82e37085db9c4f3d574bae845
alex <fancn21th@aliyun.com>
2021-07-08 21:03:41 +0800

    reset

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -16,8 +16,7 @@ Description related to library.
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml',
-        'views/templates.xml'
+        'views/library_book.xml'
     ],
 
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
