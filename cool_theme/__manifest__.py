e2111cf82d80ccf431fb0fbeb1fb87aec5be52d0
alex <fancn21th@aliyun.com>
2021-08-29 18:24:49 +0800

    layout snippet

--- cool_theme/__manifest__.py
+++ cool_theme/__manifest__.py
@@ -16,17 +16,24 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Theme/Creative',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['website'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/snippets/s_foo.xml',
+        'views/snippets/s_bar.xml',
+        'views/snippets/s_baz.xml',
+        'views/snippets/s_layout.xml',
+        'views/snippets.xml',
         'views/views.xml',
         'views/templates.xml',
+        'views/layout.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
